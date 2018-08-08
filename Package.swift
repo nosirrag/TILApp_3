0a5d897ac653f7d7d3dde1ba9a9fd5bd7c118619
ShapeShifterLab <matt@shapeshifterlab.c>
2018-08-08 01:48:17 -0400

    moving along the book

--- Package.swift
+++ Package.swift
@@ -4,16 +4,23 @@ import PackageDescription
 let package = Package(
     name: "TILApp_3",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
+        
+    // Vapor
+    .package(url: "https://github.com/vapor/vapor.git",
+    from: "3.0.0-rc"),
+    
+    // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+    
+    // Fluent PostgreSQL
+    .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
+    
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+    // 2
+    .target(name: "App", dependencies: ["FluentPostgreSQL",
+    "Vapor", "FluentSQLite"]),
+    .target(name: "Run", dependencies: ["App"]),
+    .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
 )
-
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,15 @@
+
+
+// 1 import PostgreSQL
 import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-    try services.register(FluentSQLiteProvider())
+    // 2
+    try services.register(FluentPostgreSQLProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -20,14 +25,23 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Configure a SQLite database
     let sqlite = try SQLiteDatabase(storage: .memory)
 
-    /// Register the configured SQLite database to the database config.
+    // Configure a database
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    
+    let databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: "localhost",
+        username: "vapor",
+        database: "vapor",
+        password: "password")
+    
+    let database = PostgreSQLDatabase(config: databaseConfig)
+    
+    databases.add(database: database, as: .psql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Acronym.self, database: .sqlite)
+    migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
 
 }
