a7099bb98e52c043cc21e1a78979f9be522c3afa
ShapeShifterLab <matt@shapeshifterlab.c>
2018-08-08 01:58:47 -0400

    small changes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -25,19 +25,29 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Configure a SQLite database
     let sqlite = try SQLiteDatabase(storage: .memory)
 
-    // Configure a database
+    
+    // 1
     var databases = DatabasesConfig()
-    
+    // 2
+    let hostname = Environment.get("DATABASE_HOSTNAME")
+        ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD")
+        ?? "password"
+    // 3
     let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor",
-        database: "vapor",
-        password: "password")
-    
+        hostname: hostname,
+        username: username,
+        database: databaseName,
+        password: password)
+    // 4
     let database = PostgreSQLDatabase(config: databaseConfig)
-    
+    // 5
     databases.add(database: database, as: .psql)
+    // 6
     services.register(databases)
+    
 
     /// Configure migrations
     var migrations = MigrationConfig()
