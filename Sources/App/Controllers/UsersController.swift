533507b8b8b53d4c931cc85eb5e665a870fb3250
ShapeShifterLab <matt@shapeshifterlab.c>
2018-08-27 22:26:48 -0400

    "added User model"

---
+++ Sources/App/Controllers/UsersController.swift
@@ -0,0 +1,27 @@
+import Vapor
+
+struct UsersController: RouteCollection {
+    
+    func boot(router: Router) throws {
+        let usersRoute = router.grouped("api", "users")
+        
+        usersRoute.post(User.self, use: createHandler)
+        usersRoute.get(use: getAllHndler)
+        usersRoute.get(User.parameter, use: getHandler)
+    }
+    
+    func createHandler(_ req: Request, user: User) throws -> Future<User> {
+        return user.save(on: req)
+    }
+    
+    func getAllHndler(_ req: Request) throws -> Future<[User]> {
+        return User.query(on: req).all()
+    }
+    
+    func getHandler(_ req: Request) throws -> Future<User> {
+        return try req.parameters.next(User.self)
+    }
+    
+    
+    
+}
